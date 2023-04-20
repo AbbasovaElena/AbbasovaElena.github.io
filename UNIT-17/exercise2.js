2358ace24eb62864f41ef80b9eee218767cf3cac
ALena <abb.elena@yandex.ru>
2023-04-20 21:25:16 +0500

    частичное выполнение задания 2

---
+++ UNIT-17/exercise2.js
@@ -0,0 +1,17 @@
+//var clothesGirl = "dress";
+//let clothesAll = "t-shirt";
+//const clothesBoy = "trousers";
+
+numConst = 40;
+console.log(numConst);
+strVar = 'Пока вроде бы все получается!'
+console.log(strVar);
+
+
+
+apple = {
+    form: 'round',
+    color: 'green'
+}
+console.log(apple);
+console.log(apple.color);
