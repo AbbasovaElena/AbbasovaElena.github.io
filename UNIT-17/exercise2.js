7ca0a1a3ec8aa8dfb8f12049d543b0f7bf7e91b9
ALena <abb.elena@yandex.ru>
2023-04-22 13:50:09 +0500

    Финиш ДЗ к уроку 17

--- UNIT-17/exercise2.js
+++ UNIT-17/exercise2.js
@@ -2,16 +2,29 @@
 //let clothesAll = "t-shirt";
 //const clothesBoy = "trousers";
 
-numConst = 40;
-console.log(numConst);
+const age = 40;
+console.log(age);
+
+
 strVar = 'Пока вроде бы все получается!'
 console.log(strVar);
 
-
-
-apple = {
+let apple = {
     form: 'round',
-    color: 'green'
+    color: 'green',
+    quantity: 2,
 }
 console.log(apple);
 console.log(apple.color);
+console.log(apple.quantity +1)
+console.log(apple.size)
+
+apple.size= 'big'
+console.log(apple);
+
+delete apple.form
+console.log(apple)
+
+console.log(age === apple.quantity);
+
+console.log(age != apple.quantity);
