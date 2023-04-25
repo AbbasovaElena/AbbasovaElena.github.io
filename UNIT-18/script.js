813ad3cf38b1faf590d5e01819aab362a0b23ff9
ALena <abb.elena@yandex.ru>
2023-04-25 15:21:14 +0500

    Задание 3 к уроку 18

--- UNIT-18/script.js
+++ UNIT-18/script.js
@@ -1,8 +1,11 @@
+console.log('--Задание_1--');
 let height = 170
 if (height <= 80){console.log('newborn');}
 else if(height <= 122){console.log('preschooler');}
 else if(height <= 158){console.log('schooler');}
 else{console.log('teenager or adult');}
+
+console.log('--Задание_2--');
 
 let arrClothes = ['dress', 'trousers', 'blouse', 'shorts'];
 for(let i = 0; i < arrClothes.length; i++){console.log(arrClothes[i])}
@@ -10,4 +13,23 @@ console.log('*************************');
 
 for (const iterator of arrClothes) {
     console.log(iterator);
+    }
+
+console.log('--Задание_3--');
+
+let exerciseIII = {
+    arrClothes:['dress', 'trousers', 'blouse', 'shorts'],
+    apple: {
+        form: 'round',
+        collor: 'green',
+        size: 'big'
+    },
+    text: 'Финишное задание'
 }
+for (const iterator in exerciseIII) {
+   console.log(iterator);  
+   console.log(exerciseIII[iterator]);  
+   if (iterator === 'text') {delete exerciseIII.text   
+    };}
+
+console.log(exerciseIII);
